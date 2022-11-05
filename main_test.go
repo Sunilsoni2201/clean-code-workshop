cd114d788f11f61fe3c370ba8db595d95cdd6bee
I350766 <g.anandache@sap.com>
2022-11-05 12:09:26 +0530

    added test cases

---
+++ main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func Test_traverseDir(t *testing.T) {
+	type args struct {
+		hashes     map[string]string
+		duplicates map[string]string
+		dupeSize   *int64
+		entries    []os.FileInfo
+		directory  string
+	}
+
+	files, _ := os.Stat("./test/files/")
+	dsize := int64(0)
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "testing for files",
+			args: args{
+				hashes:     map[string]string{},
+				duplicates: map[string]string{},
+				dupeSize:   &dsize,
+				entries:    []os.FileInfo{files},
+				directory:  "./test/",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			traverseDir(tt.args.hashes, tt.args.duplicates, tt.args.dupeSize, tt.args.entries, tt.args.directory)
+			fmt.Println(len(tt.args.duplicates))
+			fmt.Println(len(tt.args.hashes))
+
+			if len(tt.args.hashes) != 2 {
+				t.Error("Failure in running the test due to mismatch len in Hashes", len(tt.args.hashes))
+			}
+			if len(tt.args.duplicates) != 0 {
+				t.Error("Failure in running the test due to mismatch len in Duplicates", len(tt.args.duplicates))
+			}
+		})
+	}
+}
